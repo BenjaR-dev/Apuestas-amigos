baaa5082a393947fab5f6ff03de539bb2e6a6818
BenjaR-dev <103811160+BenjaR-dev@users.noreply.github.com>
2022-07-03 19:31:34 -0300

    Add files via upload

---
+++ script-html.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+echo start
+sleep 0.1
+echo .
+cp perdiste.html ganaste-o-no.html
+sleep 0.1
+clear
+cp ganaste.html ganaste-o-no.html
+echo -
+clear
+sleep 0.1
+echo .
+cp perdiste.html ganaste-o-no.html
+sleep 0.0
+bash script-html.sh
